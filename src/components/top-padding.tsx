5effe6391c5f70861fa3fa305d0f9cf9bdd25b22
desh-dev <etahclinton506@gmail.com>
2025-10-17 14:51:55 +0100

    fix: ios scroll 6

--- src/components/top-padding.tsx
+++ src/components/top-padding.tsx
@@ -5,7 +5,9 @@ import React from "react";
 
 const TopPadding = () => {
   const { isStandalone, isIOS } = useIsDesktop();
-  return <div>{isStandalone && isIOS && <div className="h-20"></div>}</div>;
+  return (
+    <div>{isStandalone && isIOS && <div className="h-20 pt-20"></div>}</div>
+  );
 };
 
 export default TopPadding;
